6693ebf8554aa2ee89554f1c22e9c45e9f0c81d0
Wouter Vernaillen <wouter@vernaillen.com>
2020-02-29 18:35:00 +0100

    bootstrap-vue plugin

---
+++ src/plugins/bootstrap-vue.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '../canvas/style.scss'
+
+Vue.use(BootstrapVue)
